00f4c4388f8f07221e1e6a5f64710c1f74948123
haruxime <haruxime01@gmail.com>
2025-11-08 01:04:36 +0400

    add simple authorization

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('loginForm');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('registerForm');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/password/change', [AuthController::class, 'showChangePasswordForm'])->name('changePasswordForm');
+Route::post('/password/change', [AuthController::class, 'changePassword'])->name('changePassword');
+
+Route::get('/profile', [AuthController::class, 'profile'])->name('profile');
+Route::patch('/profile', [AuthController::class, 'updateProfile'])->name('updateProfile');
+
